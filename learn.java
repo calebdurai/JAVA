bd88b5b92c975991c34fe9de9cfd13ed848cbcf6
calebdurai <calebduraibanna@gmail.com>
2024-05-08 20:06:57 +0530

    Create learn.java

---
+++ learn.java
@@ -0,0 +1,714 @@
+import java.util.Scanner;
+
+public class palin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int reverse = 0;
+        int original = number;
+
+        while (number != 0) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number /= 10;
+        }
+
+        System.out.println("Reversed number: " + reverse);
+
+        if (original == reverse) {
+            System.out.println(original + " is a palindrome number.");
+        } else {
+            System.out.println(original + " is not a palindrome number.");
+        }
+    }
+}
+
+
+public class 2waymultable {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class PowerCalculation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the base number:");
+        double base = scanner.nextDouble();
+
+        System.out.println("Enter the power:");
+        int power = scanner.nextInt();
+
+        double result = 1;
+        int count = 0;
+        while (count < power) {
+            result *= base;
+            count++;
+        }
+
+        System.out.println(base + " raised to the power of " + power + " is: " + result);
+    }
+}
+
+import java.util.Scanner;
+
+public class KrishnamoorthyNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+
+        if (isKrishnamoorthy(number)) {
+            System.out.println(number + " is a Krishnamoorthy number.");
+        } else {
+            System.out.println(number + " is not a Krishnamoorthy number.");
+        }
+    }
+
+    public static int factorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+
+    public static boolean isKrishnamoorthy(int num) {
+        int originalNumber = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += factorial(digit);
+            num /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+}
+
+
+
+import java.util.Scanner;
+
+public class KrishnamoorthyNumbersInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the starting number of the range:");
+        int start = scanner.nextInt();
+
+        System.out.println("Enter the ending number of the range:");
+        int end = scanner.nextInt();
+
+        System.out.println("Krishnamoorthy numbers within the range:");
+        for (int i = start; i <= end; i++) {
+            if (isKrishnamoorthy(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static int factorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+
+    public static boolean isKrishnamoorthy(int num) {
+        int originalNumber = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += factorial(digit);
+            num /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+}
+
+
+
+import java.util.Scanner;
+
+public class SeriesSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms:");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i += 2) {
+            sum += i;
+        }
+
+        System.out.println("Sum of the series (1+3+5+...+" + n + " terms) is: " + sum);
+    }
+}
+
+
+import java.util.Scanner;
+
+public class SeriesSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms:");
+        int n = scanner.nextInt();
+
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += (double) i / (2 * i + 1);
+        }
+
+        System.out.println("Sum of the series (1/3 + 2/5 + 3/7 + ... +" + n + " terms) is: " + sum);
+    }
+}
+
+
+import java.util.Scanner;
+
+public class SeriesSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the value of x:");
+        double x = scanner.nextDouble();
+
+        System.out.println("Enter the number of terms:");
+        int n = scanner.nextInt();
+
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            int power = 2 * i + 1;
+            double term = Math.pow(x, power) * (i % 2 == 0 ? 1 : -1);
+            sum += term;
+        }
+
+        System.out.println("Sum of the series (x - x^3 + x^5 - x^7 + ... +" + n + " terms) is: " + sum);
+    }
+}
+
+
+import java.util.Scanner;
+
+public class SineSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the value of x in radians:");
+        double x = scanner.nextDouble();
+
+        System.out.println("Enter the number of terms:");
+        int n = scanner.nextInt();
+
+        double sum = 0;
+        int sign = 1;
+        for (int i = 0; i < n; i++) {
+            int power = 2 * i + 1;
+            double term = Math.pow(x, power) / factorial(power) * sign;
+            sum += term;
+            sign *= -1;
+        }
+
+        System.out.println("Sine series value for x = " + x + " with " + n + " terms is: " + sum);
+    }
+
+    public static int factorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            int fact = 1;
+            for (int i = 2; i <= num; i++) {
+                fact *= i;
+            }
+            return fact;
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+
+        boolean isPrime = isPrime(number);
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+import java.util.Scanner;
+
+public class PrimeNumberGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the lower limit:");
+        int lowerLimit = scanner.nextInt();
+
+        System.out.println("Enter the upper limit:");
+        int upperLimit = scanner.nextInt();
+
+        System.out.println("Prime numbers within the range " + lowerLimit + " to " + upperLimit + ":");
+        for (int i = lowerLimit; i <= upperLimit; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+import java.util.Scanner;
+
+public class HollowSquare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of the square:");
+        int size = scanner.nextInt();
+
+        for (int i = 1; i <= size; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+
+        for (int i = 2; i <= size - 1; i++) {
+            System.out.print("* ");
+            for (int j = 2; j <= size - 1; j++) {
+                System.out.print("  ");
+            }
+            System.out.println("*");
+        }
+
+        if (size > 1) {
+            for (int i = 1; i <= size; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class RightHalfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class LeftHalfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  "); // Print spaces
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* "); // Print asterisks
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class FullPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            for (int l = i - 1; l >= 1; l--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class InvertedRightHalfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  "); // Print spaces
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* "); // Print asterisks
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class InvertedLeftHalfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = i; j < rows; j++) {
+                System.out.print("  "); // Print spaces
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* "); // Print asterisks
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = i; j < rows; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        int number = 1;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter the operation (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of the day (1 for Monday, 2 for Tuesday, ..., 7 for Sunday):");
+        int dayNumber = scanner.nextInt();
+
+        String day;
+        switch (dayNumber) {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            case 7:
+                day = "Sunday";
+                break;
+            default:
+                day = "Invalid day number";
+        }
+
+        System.out.println("Day of the week: " + day);
+    }
+}
+
+
+import java.util.Scanner;
+
+public class ArmstrongNumberSwitch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number to check if it's Armstrong:");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int sum = 0;
+        int digits = String.valueOf(number).length();
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, digits);
+            number /= 10;
+        }
+
+        switch (sum == originalNumber ? 1 : 0) {
+            case 1:
+                System.out.println(originalNumber + " is an Armstrong number.");
+                break;
+            case 0:
+                System.out.println(originalNumber + " is not an Armstrong number.");
+                break;
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number to calculate its factorial:");
+        int number = scanner.nextInt();
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+}
+
+
+import java.util.Scanner;
+
+public class TwoNumEquality {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter two numbers:");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        switch (num1 == num2 ? 1 : 0) {
+            case 1:
+                System.out.println("The two numbers are equal.");
+                break;
+            case 0:
+                System.out.println("The two numbers are not equal.");
+                break;
+        }
+    }
+}
+
+import java.util.Scanner;
+
+public class CheckPrimeSwitch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number to check if it's prime:");
+        int number = scanner.nextInt();
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        switch (isPrime ? 1 : 0) {
+            case 1:
+                System.out.println(number + " is a prime number.");
+                break;
+            case 0:
+                System.out.println(number + " is not a prime number.");
+                break;
+        }
+    }
+}
+
+
+
+
+
